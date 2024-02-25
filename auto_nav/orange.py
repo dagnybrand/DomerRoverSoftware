189750c10ebbfc9c3856ab53b94224702aae1bd3
henryJ099 <147459351+henryJ099@users.noreply.github.com>
2024-02-25 14:40:20 -0500

    Add files via upload

--- auto_nav/orange.py
+++ auto_nav/orange.py
@@ -18,24 +18,39 @@ while True:
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
     height, width, _ = frame.shape
+    #print(height, width)
 
-    x = range(0, height, 10)
-    y = range(0, width, 10)
-
+    x = range(0, height, 5)
+    y = range(0, width, 5)
+    xCo = -10
+    yCo = -10
     orange_ct = 0
+    lower_orange = np.array([0, 150, 150])
+    upper_orange = np.array([50, 255, 255])
 
+    mask = cv.inRange(hsv, lower_orange, upper_orange)
+    res = cv.bitwise_and(frame,frame, mask= mask)
+    '''
     for i in x:
         for j in y:
             pixel_center = hsv[i, j]
             hue_value = pixel_center[0]
             if hue_value < 50:
                 orange_ct += 1
-    
-    if orange_ct > 10000:
-        cv.circle(frame, (300, 300), 50, (255, 255, 255), 3)
+                xCo = i
+                yCo = j
+                frame[i, j] = [255,255,255]
+    '''
+    #if orange_ct > 10000:
+     #   cv.circle(frame, (xCo, yCo), 50, (255, 255, 255), 3)
+     #   cv.circle(frame, (150, 150), 50, (255,255,255), 3)
+    if xCo > 0:
+        cv.circle(frame, (xCo, yCo), 50, (255, 255, 255), 3)
         
     
     cv.imshow("frame", frame)
+    cv.imshow("mask", mask)
+    cv.imshow("res", res)
     key = cv.waitKey(1)
     if key == ord("q"):
         break
