0bdca28fb4eda33f589bb7c513e795f4a2154944
Michael <michael.lagsam@gmail.com>
2024-02-27 20:36:15 -0500

    Mike's version

---
+++ src/build/testarm/CMakeFiles/testarm_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testarm_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/testarm_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
